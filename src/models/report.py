65d7bc4db660dcd00d8ba5549a12099f52a8f6ba
George Golubev <george.gol003@gmail.com>
2022-11-05 12:00:08 +0300

    update report.py

--- src/models/report.py
+++ src/models/report.py
@@ -3,10 +3,20 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Report:
-    name: str
-    phone: int
-    email: str
-    address: str
-    rooms_count: int
+    name: str = field(default="")
+    phone: int = field(default=0)
+    email: str = field(default="")
+    address: str = field(default="")
+    rooms_count: int = field(default=0)
     rooms_before: list = field(default_factory=list)
     rooms_after: list = field(default_factory=list)
+
+    _room_index: int = field(repr=False, default=0)
+
+    @property
+    def room_index(self) -> int:
+        return self._room_index
+
+    @room_index.setter
+    def room_index(self, value: int) -> None:
+        self._room_index = value
