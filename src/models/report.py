42a2f709f2e9873ccd2df5d39fda169011e9b091
George Golubev <george.gol003@gmail.com>
2022-11-08 22:31:05 +0300

    rewrite for right intergration with pdfGenerator

--- src/models/report.py
+++ src/models/report.py
@@ -1,17 +1,20 @@
 from dataclasses import dataclass, field
+from datetime import datetime
 from .room import Room
 
 
 @dataclass
 class Report:
+    date: datetime = datetime.now()
     name: str = ""
     phone: str = ""
     email: str = ""
     address: str = ""
-    comment: str = ""
+    helped_with: str = ""
+    cleaned: str = ""
     rooms_count: int = 0
     rooms: list[Room] = field(default_factory=list)
-    additional: list = field(default_factory=list)
+    extra: list = field(default_factory=list)
 
     _room_index: int = field(repr=False, default=0)
 
@@ -22,3 +25,20 @@ class Report:
     @room_index.setter
     def room_index(self, value: int) -> None:
         self._room_index = value
+
+    def dict(self) -> dict:
+        return {
+            "Outline": {
+                "date": self.date,
+                "name": self.name,
+                "phone_number": self.phone,
+                "address": self.address,
+                "helped_with": self.helped_with,
+                "cleaned": self.cleaned,
+            },
+            "Rooms": {
+                "number_of_rooms": self.rooms_count,
+                "rooms_list": [room.dict() for room in self.rooms],
+            },
+            "Extra": self.extra,
+        }
