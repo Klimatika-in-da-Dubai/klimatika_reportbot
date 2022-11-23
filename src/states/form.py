c6bdc879d1a6c629168801524048da0c2c7ef835
George Golubev <george.gol003@gmail.com>
2022-11-23 13:39:43 +0300

    edited states

--- src/states/form.py
+++ src/states/form.py
@@ -2,16 +2,27 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class Form(StatesGroup):
+    client_name = State()
     date = State()
-    name = State()
-    phone = State()
-    email = State()
-    address = State()
+    client_type = State()
+    client_phone = State()
+    client_address = State()
+
     service = State()
     extra_service = State()
-    rooms_count = State()
-    room_type = State()
-    room_object = State()
-    room_before = State()
-    room_after = State()
-    extra = State()
+
+    room_before_vent = State()
+    room_before_duct = State()
+    room_before_pallet = State()
+    room_before_radiator = State()
+    room_before_filter = State()
+    room_before_impelers = State()
+
+    room_after_vent = State()
+    room_after_duct = State()
+    room_after_pallet = State()
+    room_after_radiator = State()
+    room_after_filter = State()
+    room_after_impelers = State()
+
+    add_room = State()
