e41ef5908879a89a9efa30138fe3f701d6911984
George Golubev <george.gol003@gmail.com>
2023-02-21 19:03:27 +0300

    cancel functionality realisation

--- src/handlers/message/users/message.py
+++ src/handlers/message/users/message.py
@@ -5,6 +5,10 @@ from aiogram.utils.i18n import gettext as _
 
 from src.models.report import Report
 from src.states.form import Form
+
+import src.misc.getters as get
+from src.states import setters as set_state
+
 from loader import users
 
 router = Router()
@@ -13,23 +17,81 @@ router = Router()
 @router.message(Command(commands=["start"]))
 async def command_start(message: types.Message, state: FSMContext) -> None:
     users[message.chat.id] = Report()
-    await state.set_state(Form.client_name)
     await message.answer(
         _("Hi! {first_name} {last_name}").format(
             first_name=message.from_user.first_name,
             last_name=message.from_user.last_name,
         )
     )
-    await message.answer(_("Enter clients Name"))
+    await set_state.set_client_name_state(message, state)
 
 
-@router.message(Command(commands=["cancel"]))
-@router.message(F.text.casefold() == "cancel")
-async def cancel_handler(message: types.Message, state: FSMContext) -> None:
-    current_state = await state.get_state()
+@router.message(Form.date, Command(commands=["cancel"]))
+async def cancel_date(message: types.Message, state: FSMContext) -> None:
+    await set_state.set_client_name_state(message, state)
 
-    if current_state is None:
+
+@router.message(Form.client_phone, Command(commands=["cancel"]))
+async def cancel_client_phone(message: types.Message, state: FSMContext) -> None:
+    await set_state.set_date_state(message, state)
+
+
+@router.message(Form.client_address, Command(commands=["cancel"]))
+async def cancel_client_address(message: types.Message, state: FSMContext) -> None:
+    await set_state.set_client_phone_state(message, state)
+
+
+@router.message(Form.service, Command(commands=["cancel"]))
+async def cancel_service(message: types.Message, state: FSMContext) -> None:
+    await set_state.set_client_address_state(message, state)
+
+
+@router.message(Form.extra_service, Command(commands=["cancel"]))
+async def cancel_extra_service(message: types.Message, state: FSMContext) -> None:
+    await set_state.set_service_state(message, state)
+
+
+@router.message(Form.room_cleaning_nodes, Command(commands=["cancel"]))
+async def cancel_room_cleaning_nodes(message: types.Message, state: FSMContext) -> None:
+    report = get.get_current_user_report(message.chat.id)
+
+    if len(report.rooms) > 1:
+        report.rooms.pop()
+        await set_state.set_add_room_state(message, state)
         return
 
-    await state.clear()
-    await message.answer(_("Cancelled!"))
+    if report.service == Report.Service.PREMIUM_EXTRA:
+        await set_state.set_extra_service_state(message, state)
+    else:
+        await set_state.set_service_state(message, state)
+
+
+@router.message(Form.cleaning_node_img_before, Command(commands=["cancel"]))
+async def cancel_img_before(message: types.Message, state: FSMContext) -> None:
+    room = get.get_current_user_room(message.chat.id)
+
+    if room._index == 0:
+        await set_state.set_room_cleaning_nodes_state(message, state)
+    else:
+        room.nodes_queue_back()
+        await set_state.set_img_before_state(message, state)
+
+
+@router.message(Form.cleaning_node_img_after, Command(commands=["cancel"]))
+async def cancel_img_after(message: types.Message, state: FSMContext) -> None:
+    room = get.get_current_user_room(message.chat.id)
+
+    if room._index == 0:
+        room._index = len(room.nodes_queue) - 1
+        await set_state.set_img_before_state(message, state)
+    else:
+        room.nodes_queue_back()
+        await set_state.set_img_after_state(message, state)
+
+
+@router.message(Form.add_room, Command(commands=["cancel"]))
+async def cancel_add_room(message: types.Message, state: FSMContext) -> None:
+    room = get.get_current_user_room(message.chat.id)
+    room.nodes_queue_back()
+    await set_state.set_img_after_state(message, state)
+
---
+++ src/states/setters.py
@@ -0,0 +1,98 @@
+from aiogram import types
+from aiogram.fsm.context import FSMContext
+
+from aiogram.utils.i18n import gettext as _
+
+from src.states import Form
+from src.keyboards import inline
+from src.misc import getters as get
+
+from src.models import Report, CleaningNode
+
+
+async def set_client_name_state(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(Form.client_name)
+    await message.answer(_("Enter clients Name"))
+
+
+async def set_date_state(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(Form.date)
+    await message.answer(
+        _(
+            "Enter date of visit in format DAY MONTH YEAR.\n\nFor example: 1 12 2022 (December the 1st, 2022)"
+        )
+    )
+
+
+async def set_client_phone_state(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(Form.client_phone)
+    await message.answer(_("Enter Phone number"))
+
+
+async def set_client_address_state(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(Form.client_address)
+    await message.answer(_("Enter client address:"))
+
+
+async def set_service_state(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(Form.service)
+    await inline.send_service_keyboard(message)
+
+
+async def set_extra_service_state(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(Form.extra_service)
+    await inline.send_extra_service_keyboard(message)
+
+
+async def set_room_cleaning_nodes_state(
+    message: types.Message, state: FSMContext
+) -> None:
+    await state.set_state(Form.room_cleaning_nodes)
+    await inline.send_cleaning_node_keyboard(message)
+
+
+async def set_state_room_cleaning_nodes(message: types.Message, state: FSMContext):
+    report = get.get_current_user_report(message.chat.id)
+    if report.service != Report.Service.OTHER_REPAIR_SERVICES:
+        set_default_cleaning_nodes(message)
+    await state.set_state(Form.room_cleaning_nodes)
+    await inline.send_cleaning_node_keyboard(message)
+
+
+def set_default_cleaning_nodes(message: types.Message):
+    DEFAULT_CLEANING_NODES = [
+        CleaningNode("grills", button_text=_("grills"), type=CleaningNode.Type.DEFAULT),
+        CleaningNode("duct", button_text=_("duct"), type=CleaningNode.Type.DEFAULT),
+        CleaningNode("pan", button_text=_("pan"), type=CleaningNode.Type.DEFAULT),
+        CleaningNode(
+            "radiator",
+            button_text=_("radiator"),
+            type=CleaningNode.Type.DEFAULT,
+        ),
+        CleaningNode("filter", button_text=_("filter"), type=CleaningNode.Type.DEFAULT),
+        CleaningNode("blades", button_text=_("blades"), type=CleaningNode.Type.DEFAULT),
+    ]
+    room = get.get_current_user_room(message.chat.id)
+    for node in DEFAULT_CLEANING_NODES:
+        room.set_default_node(node)
+
+
+async def set_img_before_state(message: types.Message, state: FSMContext) -> None:
+    room = get.get_current_user_room(message.chat.id)
+    await state.set_state(Form.cleaning_node_img_before)
+    await message.answer(
+        _("Send photo BEFORE for") + " " + room.current_node.button_text
+    )
+
+
+async def set_img_after_state(message: types.Message, state: FSMContext) -> None:
+    room = get.get_current_user_room(message.chat.id)
+    await state.set_state(Form.cleaning_node_img_after)
+    await message.answer(
+        _("Send photo AFTER for") + " " + room.current_node.button_text
+    )
+
+
+async def set_add_room_state(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(Form.add_room)
+    await inline.send_yes_no_keboard(message, _("Do you want to add room?"))
