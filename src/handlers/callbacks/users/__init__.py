50c67ef5b2b852670289cbfda52393110b0deb33
George Golubev <george.gol003@gmail.com>
2023-02-28 20:49:57 +0300

    added new logic in getting photos for repair works

--- src/handlers/callbacks/users/__init__.py
+++ src/handlers/callbacks/users/__init__.py
@@ -1,6 +1,8 @@
 from aiogram import Router
 from .form import router
+from .repair_works import router as repair_works_router
 
 users_router = Router()
 
 users_router.include_router(router)
+users_router.include_router(repair_works_router)
